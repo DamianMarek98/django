f5c43ee5112f3e984dedb8606db414aa1507010c
Damian Marek <zmdddamian@gmail.com>
2021-05-25 17:25:46 +0200

    allowed hosts

--- league/settings.py
+++ league/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^_lswttcri&o!6@8$ja=8w*0+a5&)!awxz(q*z$jc3r$j0%0^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'league-django-js.herokuapp.com']
 
 
 # Application definition
